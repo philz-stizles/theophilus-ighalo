18514ef096a22bfca51aa70cc0f8163ad85b010f
Theophilus Ighalo <theophilusighalo@gmail.com>
2023-07-28 19:30:20 +0100

    add blog layout

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,31 @@
+import '@/app/globals.css';
+import { Raleway } from 'next/font/google';
+import { Footer, Header, Navbar } from '@/components/layout';
+import { ScrollButton } from '@/components/ui';
+
+const inter = Raleway({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700'],
+});
+
+export const metadata = {
+  title: 'Theophilus Ighalo',
+  description: 'Online portfolio',
+};
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+        <ScrollButton />
+      </body>
+    </html>
+  );
+}
